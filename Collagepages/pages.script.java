50e0259f269b50c2dc14ae4ba0cef7919d9826bf
aliwhitewustl <36942918+aliwhitewustl@users.noreply.github.com>
2018-07-23 09:34:52 +0200

    Collection first commit

---
+++ Collagepages/pages.script.java
@@ -0,0 +1,23 @@
+$(document).ready(function(){   
+
+
+console.log("test");
+
+
+
+
+
+
+$(".imagetemplate").mouseenter(function(){
+		$(this).fadeTo( "fast", 0 );
+});
+
+$(".imagetemplate").mouseleave(function(){
+		$(this).fadeTo( "fast", 1 );
+});
+
+
+
+
+
+	});  
---
+++ script.js
@@ -0,0 +1,163 @@
+$(document).ready(function(){   
+
+
+console.log("test");
+
+
+
+
+
+$("#gridicon").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#gridicon").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+$("#timelineicon").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#timelineicon").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+$("#worldicon").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#worldicon").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+
+
+
+$("#gridiconT").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#gridiconT").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+$("#timelineiconT").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#timelineiconT").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+$("#worldiconT").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#worldiconT").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+
+
+
+$("#gridiconM").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#gridiconM").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+$("#timelineiconM").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#timelineiconM").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+$("#worldiconM").mouseenter(function(){
+		$(this).fadeTo( "fast", 1.0 );
+});
+$("#worldiconM").mouseleave(function(){
+		$(this).fadeTo( "fast", .7 );
+});
+
+
+
+
+
+
+
+$("#scrap1").click(function(){
+		$(this).animate({left: "-=150", top: "+=150"}, 1000);
+});
+
+
+$("#scrap2").click(function(){
+		$(this).animate({left: "+=150", top: "+=150"}, 1000);
+});
+
+$("#scrap3").click(function(){
+		$(this).animate({top: "+=150"}, 1000);
+});
+
+
+$("#scrap4").click(function(){
+		$(this).animate({top: "-=150", left: "+=150"}, 1000);
+});
+
+$("#scrap5").click(function(){
+		$(this).animate({top: "-=150",}, 1000);
+});
+
+$("#scrap6").click(function(){
+		$(this).animate({top: "+=150",}, 1000);
+});
+
+
+// $( "#gridicon" ).mouseenter(function() {
+//   $(this).animate({opacity: 1})
+// });
+
+// $( "#gridicon" ).mouseleave(function() {
+//   $(this).animate({opacity: .7})
+// });
+
+
+// $( "#timelineicon" ).mouseenter(function() {
+//   $(this).animate({opacity: 1}, 0 )
+// });
+// $( "#timelineicon" ).mouseleave(function() {
+//   $(this).animate({opacity: .7}, 0 )
+// });
+
+
+
+// $( "#worldicon" ).mouseenter(function() {
+//   $(this).animate({opacity: 1}, 0 )
+// });
+// $( "#worldicon" ).mouseleave(function() {
+//   $(this).animate({opacity: .7}, 0 )
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	});  
